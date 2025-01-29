eba9cd487131125c1eec34af1e490857e3d1a179
Rayan <rayanlupo@gmail.com>
2025-01-29 17:30:48 +0100

    new error :)

--- src/main/java/net/rayandev/specialtridents/item/ModItems.java
+++ src/main/java/net/rayandev/specialtridents/item/ModItems.java
@@ -2,38 +2,59 @@ package net.rayandev.specialtridents.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.TridentItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.rayandev.specialtridents.SpecialTridents;
-import net.rayandev.specialtridents.item.custom.FireTridentItem;
+
 
 public class ModItems {
 
-    // Define item group key
+
+        public static Item register(Item item, RegistryKey<Item> registryKey) {
+            Item registeredItem = Registry.register(Registries.ITEM, registryKey.getValue(), item);
+            return registeredItem;
+        }
+
+    public static final RegistryKey<Item> TRIDENT_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SpecialTridents.MOD_ID, "fire_trident"));
     public static final RegistryKey<ItemGroup> TRIDENTS_GROUP_KEY = RegistryKey.of(
             Registries.ITEM_GROUP.getKey(),
             Identifier.of(SpecialTridents.MOD_ID, "item_group")
     );
 
-    // Register Fire Trident item
-    public static final Item FIRE_TRIDENT = new FireTridentItem(new Item.Settings()
+
+    public static final Item FIRE_TRIDENT = register(new Item(new Item.Settings()
             .rarity(Rarity.RARE)
             .maxDamage(250)
-    );
+            .attributeModifiers(TridentItem.createAttributeModifiers())
+            .component(DataComponentTypes.TOOL, TridentItem.createToolComponent())
+            .enchantable(1)
+    .maxCount(1)
+            .fireproof()
+            .registryKey(TRIDENT_KEY)), TRIDENT_KEY);
+
+
+
 
     public static void registerModItems() {
+        Identifier id = Identifier.of("specialtridents", "fire_trident");
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+
+        Item.Settings settings = new Item.Settings().registryKey(key);
+
         // Log registration
         SpecialTridents.LOGGER.info("Registering Mod Items for " + SpecialTridents.MOD_ID);
 
-        // Register the Fire Trident item
-        Registry.register(Registries.ITEM,Identifier.of(SpecialTridents.MOD_ID, "fire_trident"), FIRE_TRIDENT);
+
 
         // Register item group
         ItemGroup tridents = FabricItemGroup.builder()
--- src/main/java/net/rayandev/specialtridents/item/custom/FireTridentItem.java
+++ src/main/java/net/rayandev/specialtridents/item/custom/FireTridentItem.java
@@ -6,11 +6,13 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ProjectileItem;
 import net.minecraft.item.TridentItem;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Position;
 import net.minecraft.world.World;
@@ -18,8 +20,12 @@ import net.minecraft.world.World;
 
 public class FireTridentItem extends TridentItem{
 
+    Identifier id = Identifier.of("specialtridents", "fire_trident");
+    RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
 
-    public FireTridentItem(Item.Settings settings) {
+    Item.Settings settings = new Item.Settings().registryKey(key);
+
+    public FireTridentItem(Item.Settings settings, RegistryKey<Item> key) {
         super(settings);
     }
 

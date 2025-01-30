d043a074233b35ba3c4bbbd498d5314c61fd827b
Rayan <rayanlupo@gmail.com>
2025-01-30 23:02:03 +0100

    I GAVE UP, new item but texture still  not loading

--- src/main/java/net/rayandev/specialtridents/entity/client/FireTridentEntityRenderState.java
+++
@@ -1,9 +0,0 @@
-package net.rayandev.specialtridents.entity.client;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.state.TridentEntityRenderState;
-
-public class FireTridentEntityRenderState extends TridentEntityRenderState {
-    public FireTridentEntityRenderState(EntityRendererFactory.Context context) {
-        super();
-    }
-}
